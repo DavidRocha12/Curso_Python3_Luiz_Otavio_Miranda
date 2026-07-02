2016062a45f10dc4e86b99aa5dc61db4adb4fcde
DavidRocha <davidedisondarocha1202@gmail.com>
2026-07-01 22:18:16 -0300

    atualizando exercício

--- exercicio17_gerador_de_tabuada_while.py
+++ exercicio17_gerador_de_tabuada_while.py
@@ -1,8 +1,10 @@
 """
 Exercício: O Gerador de Tabuadas (while aninhado)
+
 Desafio:
+
 Escreva um programa em Python que imprima as tabuadas de multiplicação do 1 ao 5. Para cada um desses números,
- o programa deve mostrar a multiplicação de 1 a 10.
+o programa deve mostrar a multiplicação de 1 a 10.
 
 A regra de ouro aqui é: você não pode usar o laço for. A solução deve ser construída obrigatoriamente usando 
 um laço while dentro de outro laço while.
@@ -34,3 +36,16 @@ while externo recomeçar um ciclo, senão a segunda tabuada não vai imprimir na
 Pode tentar montar o código e jogar aqui para eu dar uma olhada. Se o código travar em um loop infinito, não se preocupe,
 faz parte do aprendizado com o while!
 """
+
+print("Tabuada do 1 ao 5")
+print("=" * 18)
+contador = 0
+while contador < 5:
+    contador += 1
+    contador_2 = 0
+    while contador_2 < 10:
+        contador_2 += 1
+        multi = contador * contador_2
+        print(f"{contador} X {contador_2:>2} = {multi} ")
+    print("=" * 12)
+print("Tabuada do 1 ao 5 concluido!")
